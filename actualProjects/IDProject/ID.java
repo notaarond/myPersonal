1139f99665c44c5db7676b362045635f5e9169cf
notaarond <decastro.aaron2020@gmail.com>
2019-09-06 21:02:25 -0700

    refined ID.java and added more commands to reference doc

--- actualProjects/IDProject/ID.java
+++ actualProjects/IDProject/ID.java
@@ -9,16 +9,18 @@ public class ID
   {
     //Program Variables
     Scanner userIn = new Scanner(System.in);
-    IDHolder[] people = new IDHolder[5];
+    int arrayMax = 5;
+    IDHolder[] people = new IDHolder[arrayMax];
     int namesCounter = 0;
     int decide = 1;
 
-    System.out.println("Welcome to the ID 'Database'!");
+    System.out.println("||| Welcome to the ID 'Database'! |||\n");
 
     //Name Entry Algorithm
     do
     {
-      if(namesCounter == 5)
+      //escape statement for maxed out array
+      if(namesCounter == arrayMax)
       {
         System.out.println("That's the limit of names you can enter!");
         break;
@@ -26,17 +28,14 @@ public class ID
 
       //Takes in user input via scanner and uses it to initialize array items
       System.out.print("Enter your name: ");
-      people[namesCounter] = new IDHolder(userIn.nextLine());
-      //people[namesCounter].setName(userIn.nextLine());
+      String name = userIn.next();
+      people[namesCounter] = new IDHolder(name); //creates a new IDHolder object that takes in user input into its constructor
       namesCounter++;
 
-    
-        //1 = input more names, 0 = end do-while, input must be 1 or 0
-        //needs to be fixed so that only 1 or 0 can be used
-        System.out.print("Will you enter more names, limit is 5?(Y = 1/N = 0): ");
-        decide = userIn.nextInt();
-
-
+      //1 = input more names, 0 = end do-while, input must be 1 or 0
+      //needs to be fixed so that only 1 or 0 can be used
+      System.out.print("Will you enter more names, limit is 5?(Y = 1/N = 0): ");
+      decide = userIn.nextInt();
 
       //if-statement that decides if the do-while statement runs again(removed for simplicity)
 
