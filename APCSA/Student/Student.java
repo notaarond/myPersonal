2a764469a6c23251373d024813442817eba999bd
notaarond <decastro.aaron2020@gmail.com>
2019-09-10 14:59:33 -0700

    IC assignment finished 9/10

--- APCSA/Student/Student.java
+++ APCSA/Student/Student.java
@@ -20,19 +20,33 @@ public class Student {
       test2 = 0;
       test3 = 0;
    }
-    
+
+   public Student(String nm, int tOne, int tTwo, int tThree){
+     name = nm;
+     test1 = tOne;
+     test2 = tTwo;
+     test3 = tThree;
+   }
+
+   public Student(Student s){
+     name = s.getName();
+     test1 = s.getScore(1);
+     test2 = s.getScore(2);
+     test3 = s.getScore(3);
+   }
+
    //Other methods
 
    public void setName (String nm){
    //Set a student's name
       name = nm;
    }
-    
+
    public String getName (){
    //Get a student's name
       return name;
    }
-  
+
    public void setScore (int i, int score){
    //Set test i to score
       if      (i == 1) test1 = score;
@@ -46,14 +60,14 @@ public class Student {
       else if (i == 2) return test2;
       else             return test3;
    }
-   
+
    public int getAverage(){
    //Compute and return the average
       int average;
       average = (int) Math.round((test1 + test2 + test3) / 3.0);
       return average;
    }
-    
+
    public int getHighScore(){
    //Determine and return the highest score
       int highScore;
@@ -62,15 +76,34 @@ public class Student {
       if (test3 > highScore) highScore = test3;
       return highScore;
    }
-    
+
+   public String validateData(){
+     String errorMessage = null;
+     if(name == "")
+     {
+       errorMessage = "Sorry: Name Required";
+     }
+     if(( test1 > 100 || test1 < 0 ) || (test2 > 100 || test2 < 0) || (test3 > 100 || test3 < 0) )
+     {
+       if(name == ""){
+          errorMessage += "\nSorry: must have 0 <= test score <= 100";
+        }
+       else
+       {
+         errorMessage = "Sorry: must have 0 <= test score <= 100";
+       }
+     }
+     return errorMessage;
+   }
+
    public String toString(){
    //Construct and return a string representation of the student
       String str;
       str = "Name:    " + name  + "\n" +    // "\n" denotes a newline
             "Test 1:  " + test1 + "\n" +
-            "Test 2:  " + test2 + "\n" + 
+            "Test 2:  " + test2 + "\n" +
             "Test 3:  " + test3 + "\n" +
             "Average: " + getAverage();
       return str;
-   }  
+   }
 }
--- APCSA/Student/TestStudent.java
+++ APCSA/Student/TestStudent.java
@@ -1,7 +1,7 @@
 public class TestStudent{
 
    public static void main (String[] args){
-      Student s1, s2;
+      Student s1, s2, s3;
       String str;
       int i;
 
@@ -13,10 +13,49 @@ public class TestStudent{
       System.out.println("\nHere is student s1\n" + s1);
 
 
-      s2 = s1;               // s1 and s2 now refer to the same object
-      s2.setName ("Ann");    // Set the name through s2
-      System.out.println ("\nName of s1 is now: " + s1.getName());
+      // Validating new Constructors
 
-      System.out.println(s2);
+      //Constructor with parameters of strings and ints (without using mutator methods)
+      s2 = new Student("Ann", 89, 97, 92);
+      System.out.println("\nHere is student s2\n" + s2.getName());
+      System.out.println("Her test scores are: " + s2.getScore(1) + " " + s2.getScore(2) + " " + s2.getScore(3));
+
+      //Constructors with another Student object as parameter
+      s3 = new Student(s2);
+      System.out.println("\nHere is student s3\n" + s3.getName());
+      System.out.println("Her test scores are: " + s3.getScore(1) + " " + s3.getScore(2) + " " + s3.getScore(3));
+
+      //Validation of separate references to objects
+      s3.setName("Alexandra");
+      System.out.println("s3 got a name change and is now: " + s3.getName());
+      System.out.println("While s2 is still: " + s2.getName());
+
+      //Testing validateData method
+
+      String result = s3.validateData();
+      if (result == null)
+      {
+        System.out.println("s3 is all good!\n");
+      }
+      else
+      {
+        System.out.println(result);
+        System.out.println("\n");
+      }
+
+      Student s4 = new Student();
+      //s4.setName("Hi");
+      s4.setScore(1, -5);
+
+      result = s4.validateData();
+      if (result == null)
+      {
+        System.out.println("s4 is all good!");
+      }
+      else
+      {
+        System.out.println(result);
+      }
+
    }
 }
