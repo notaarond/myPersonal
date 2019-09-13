ca0bf93bd71ed82ad25ef81e95eb7d057d6c1deb
notaarond <decastro.aaron2020@gmail.com>
2019-09-13 14:37:44 -0700

    Fraction class with Math methods and no undefined fraction test implemented

--- APCSA/Fraction/Fraction.java
+++ APCSA/Fraction/Fraction.java
@@ -1,4 +1,5 @@
-//091219 AaDeCa
+//091219 Aaron De Castro
+import java.util.Scanner;
 
 public class Fraction
 {
@@ -6,15 +7,24 @@ public class Fraction
   private int numerator;
   private int denominator;
 
-  //Constructor
-
+  //Constructor with rejection algorithm for undefined fractions( number divided by 0 )
   public Fraction(int n, int d)
   {
+    int anotherD = d; //set placeholder var for d
     numerator = n;
-    denominator = d;
+
+    //if passed denom. = 0, don't set denom. to 0, wait till user inputs a legal input
+    //while loop used so that code repeats until legal value is passed and so that code does not run if legal input initially passed
+    while(anotherD == 0){
+      System.out.println("Please enter a denominator that is not 0: ");
+      Scanner input = new Scanner(System.in);
+      anotherD = input.nextInt();
+    }
+
+    denominator = anotherD;
   }
 
-  //behaviors: get/set and should be
+  //behaviors: get/set and helper methods should be add, subtract, multiply, and divide
 
   //accessor methods
   public int getNumerator() { return numerator; }
@@ -25,14 +35,48 @@ public class Fraction
   public void setDenominator(int d) { denominator = d; }
 
   //helper methods
+  //implemented math functions to how book advised to add and subtract fractions
   public Fraction add(Fraction f)
-  {
+  { //(n1*d2) + (n2*d1)/(d1*d2)
     int newNum = (numerator * f.getDenominator()) + (f.getNumerator() * denominator);
     int newDen = (denominator * f.getDenominator());
 
+    //Instantiate new Fraction object with calculated numerator and denominator
     Fraction sum = new Fraction(newNum, newDen);
     return sum;
   }
+
+  public Fraction subtract(Fraction f)
+  {//(n1*d2) - (n2*d1)/(d1*d2)
+    int newNum = (numerator * f.getDenominator()) - (f.getNumerator() * denominator);
+    int newDen = (denominator * f.getDenominator());
+
+    //Instantiate new Fraction object with calculated numerator and denominator
+    Fraction sum = new Fraction(newNum, newDen);
+    return sum;
+  }
+
+  public Fraction multiply(Fraction f)
+  { //(n1*n2) / (d1*d2)
+    int newNum = (numerator * f.getNumerator());
+    int newDen = (denominator * f.getDenominator());
+
+    //Instantiate new Fraction object with calculated numerator and denominator
+    Fraction sum = new Fraction(newNum, newDen);
+    return sum;
+  }
+
+  public Fraction divide(Fraction f)
+  { //(n1*d2) / (d1*n2)
+    int newNum = (numerator * f.getDenominator());
+    int newDen = (denominator * f.getNumerator());
+
+    //Instantiate new Fraction object with calculated numerator and denominator
+    Fraction sum = new Fraction(newNum, newDen);
+    return sum;
+  }
+
+  //public Fraction simplify(){}
 
   public String toString()
   {
--- APCSA/Fraction/FractionTest.java
+++ APCSA/Fraction/FractionTest.java
@@ -1,15 +1,34 @@
-//091219 AaDeCa
+//091219 Aaron De Castro
+import java.util.Scanner;
 
 public class FractionTest
 {
   public static void main(String[] args) {
 
-    Fraction myFrac = new Fraction(1, 2);
+    //Instantiate first fraction class, Scanner object, and user values
+    Fraction f1 = new Fraction(1, 2);
+    Scanner userInput = new Scanner(System.in);
+    int userNum, userDen;
 
-    System.out.println(myFrac.getNumerator());
-    System.out.println(myFrac.getDenominator());
+    //User inputs Numerator
+    System.out.println("Please enter a numerator: ");
+    userNum = userInput.nextInt();
 
-    System.out.println(myFrac);
+    //User inputs denominator
+    System.out.println("Please enter a denominator: ");
+    userDen = userInput.nextInt();
 
+    //Instantiate new fraction object based on user's inputs
+    Fraction f2 = new Fraction(userNum, userDen);
+
+    //Displays the fractions to be manipulated first
+    System.out.println("Fraction 1: \n" + f1);
+    System.out.println("\nFraction 2: \n" + f2);
+
+    //Testing math methods on Fraction objects
+    System.out.println("\nAdding both: \n" + f1.add(f2));
+    System.out.println("\nSubtracting both: \n" + f1.subtract(f2));
+    System.out.println("\nMultiplying both: \n" + f1.multiply(f2));
+    System.out.println("\nDividing both: \n" + f1.divide(f2));
   }
 }
